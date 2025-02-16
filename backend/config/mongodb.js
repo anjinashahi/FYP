42706c22e66d49a192bf2cf320098f6bc8c57106
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2025-02-16 13:02:57 +0545

    gitignore

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -14,7 +14,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
-        console.log("Database connected successfully");
+        console.log("sDatabase connected successfully");
     } catch (error) {
         console.error(" Database connection failed:", error);
         process.exit(1);
