bf097c57088616f05babd6ed8523f0343ae46029
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2025-02-18 19:53:31 +0545

    admin controller

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -6,26 +6,27 @@ import {v2 as cloudinary} from "cloudinary";
 const addDoctor = async (req, res) => {
     try{
         const {name, email, password, speciality, degree, experience, about, fees, address} = req.body;
+        // console.log(req.body)
         const imageFile = req.file
 
-        console.log({name, email, password, speciality, degree, experience, about, fees, address, image})
+        console.log({name, email, password, speciality, degree, experience, about, fees, address}, imageFile)
 
-        if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address || !imageFile){
-            return res.json({success: false, message: "Missing required parameters"})
-        }
-        if (!validator.isEmail(email)) {
-            return res.json({ success: false, message: "Invalid email" });
-        }
-        if (password.length < 6) {
-            return res.json({ success: false, message: "Password must be at least 6 characters long" });
-        }
-        //hasing doc password
-        const salt = await bycrypt.genSalt(10);
-        const hashedPassword = await bycrypt.hash(password, salt);
+        // if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address || !imageFile){
+        //     return res.json({success: false, message: "Missing required parameters"})
+        // }
+        // if (!validator.isEmail(email)) {
+        //     return res.json({ success: false, message: "Invalid email" });
+        // }
+        // if (password.length < 6) {
+        //     return res.json({ success: false, message: "Password must be at least 6 characters long" });
+        // }
+        // //hasing doc password
+        // const salt = await bycrypt.genSalt(10);
+        // const hashedPassword = await bycrypt.hash(password, salt);
 
-        //uplaod image to cloudinary
-        const uploadedImage = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"});
-        const imageUrl = iamgeUpload.secure_url;
+        // //uplaod image to cloudinary
+        // const uploadedImage = await cloudinary.uploader.upload(imageFile.path, {resource_type: "image"});
+        // const imageUrl = iamgeUpload.secure_url;
     }
     catch(error){
 
