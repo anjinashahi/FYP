0177ce7c417345155a971b6cb8700578894c21dd
Anjina Shahi <np03cs4a220179@heraldcollege.edu.np>
2025-03-26 22:18:39 +0545

    doc list

--- frontend/fyp/src/context/AdminContext.jsx
+++ frontend/fyp/src/context/AdminContext.jsx
@@ -1,13 +1,28 @@
 import { createContext} from 'react';
 import {doctors} from '../assets/assets'
-
+import axios from 'axios';
+import {toast} from 'react-toastify'
+import {useState} from 'react'
 export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const[doctors, setDoctors] = useState([])
+    const getAllDoctors = async () => {
+        try{
+            const{data} = await axios.post(backendUrl +'api/admin/all-doctors', {})
+            if(data.success){
+                setDoctors(data.doctors)
+            }else{
+                toast.error(data.message)
+            }
+        }catch(error){
+            toast.error(error.message)
+            console.log(error)
+    }}
     const value = {
         doctors,
-        backendUrl
+        backendUrl, doctors, getAllDoctors
     }
     return (
         <AdminContext.Provider value={value}>
